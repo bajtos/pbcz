fd86763a6c29404e2fb5ea49a20eeb0ea924188c
Miroslav Bajtoš <miro.bajtos@gmail.com>
2013-02-17 11:13:56 +0100

    Created scripts for budget class slice.

---
+++ preprocess-data.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import os
+import sqlite3 as sql
+from helpers import *
+
+dbfile = 'data/amounts.db'
+
+def createdb(db):
+    db.execute('CREATE TABLE data (Municipality INT, class1 INT, class2 INT, class3 INT, class4 INT, year INT, amount BIGINT)')
+
+def process_item(db, item, year):
+    db.execute('INSERT INTO data VALUES(?, ?, ?, ?, ?, ?, ?)',
+       (int(item[1]), # municipality
+       int(item[5]), int(item[7]), int(item[9]), int(item[11]), # classes
+       year,
+       long(float(item[-1]))))
+
+def dropdb(dbfile):
+    try: os.remove(dbfile)
+    except OSError: pass
+
+dropdb(dbfile)
+with sql.connect(dbfile) as db:
+    createdb(db)
+    foreach_budget_item(lambda item, year: process_item(db, item, year))
+
