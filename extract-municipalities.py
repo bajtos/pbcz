3f2232f70e4be3fcded866d0c1ac5d27f7453583
Miroslav Bajtoš <miro.bajtos@gmail.com>
2013-02-17 09:33:29 +0100

    Implemented extract-municipalities.py.

---
+++ extract-municipalities.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from helpers import *;
+
+valuesDict = { } 
+
+def extract_municipality(item):
+    mid = item[1]
+    name = item[0]
+    if mid in valuesDict: return;
+    item = [mid, name]
+    valuesDict[mid] = item
+
+foreach_budget_item(extract_municipality)
+save_dimension_values('pbcz-municipalities.csv', ['municipality', 'name_cs'], valuesDict)
+
